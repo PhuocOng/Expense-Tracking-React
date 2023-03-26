0b4fe33d4149dded911ff235c16720bbb8df6d78
Peter Ong <onggiaphuoc2004@gmail.com>
2023-03-26 10:28:41 -0400

    update test

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,7 +15,21 @@ const Expenses = (props) => {
   let newExpense = props.items.filter((item) => ( //convert props.item array into newExpense array with filter is YEAR 
     item.date.getFullYear().toString() === filteredYear
   ));
-
+  
+  let showExpense; // Declare the variable outside the if and else if blocks
+  if (newExpense.length === 0) {
+    showExpense = <p>There is no expense here</p>;
+  }  
+  else if (newExpense.length>0) {
+    showExpense = newExpense.map((expense) => ( //use map to convert newExpense element into <ExpenseItem>
+    <ExpenseItem
+      id={expense.id}
+      title={expense.title}
+      amount={expense.amount}
+      date={expense.date}
+    />
+  ))
+  }
   return (
     <div>
       <Card className='expenses'>
@@ -23,15 +37,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-
-        {newExpense.map((expense) => ( //use map to convert newExpense element into <ExpenseItem>
-          <ExpenseItem
-            id = {expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {showExpense}
       </Card>
     </div>
   );
