59fd875536b770c027772dcc6ce3ae2d077888e6
Peter Ong <onggiaphuoc2004@gmail.com>
2023-03-31 15:55:16 -0400

    commit 03/31/2023

---
+++ src/components/Chart/ChartBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./ChartBar.css"
+
+
+const ChartBar = (props) => {
+    return (
+        <div className="chart-bar">
+            <div className="chart-bar__inner">
+                <div className="chart-bar__fill"></div>
+                <div className="chart-bar__label"></div>
+            </div>
+        </div>
+    )
+}
+
+export default ChartBar;
